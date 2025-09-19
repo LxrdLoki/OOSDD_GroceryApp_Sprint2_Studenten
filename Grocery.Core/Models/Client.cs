649c14fc76f338aaaa27cfc017932a95a5174828
LxrdLoki <melvinVD69@outlook.com>
2025-09-19 13:27:06 +0200

    fixed pipeline warnings for Client/ and GroceryListItemsRepository.cs by getting rid of possible null reference's

--- Grocery.Core/Models/Client.cs
+++ Grocery.Core/Models/Client.cs
@@ -3,32 +3,39 @@ namespace Grocery.Core.Models
 {
     public partial class Client : Model
     {
-        private string _emailAddress;
-        private string _password;
+        private string? _emailAddress;
+        private string? _password;
 
-        public string Password
+        public string? Password
         {
             get => _password;
             set
             {
-                // Kan hier eventueel validatie aan toevoegen
-                _password = value;
+                // Could add validation here
+                _password = value!;
             }
         }
 
-        public string EmailAddress
+        public string? EmailAddress
         {
             get => _emailAddress;
             set
             {
-                // Kan hier eventueel validatie aan toevoegen
-                _emailAddress = value;
+                // Could add validation here
+                _emailAddress = value!;
             }
         }
-        public Client(int id, string  name, string emailAddress, string password) : base(id, name)
+        public Client(int id, string name, string emailAddress, string password) : base(id, name)
         {
-            EmailAddress = emailAddress;
+            // Could add a check here to ensure emailAddress and password are not null or empty
+            if (string.IsNullOrWhiteSpace(emailAddress) || string.IsNullOrWhiteSpace(password))
+            {
+                throw new ArgumentException("Fields cannot be null or empty", nameof(emailAddress));
+            }
+
             Password = password;
+            EmailAddress = emailAddress;
+
         }
     }
 }
